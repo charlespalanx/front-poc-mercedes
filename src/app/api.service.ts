6cd61de0dd3dbe1d6a8d086908b234663652a9dc
Carlos Palanca Flaque <carlos.palanca@entelgy-ibai.com>
2024-03-04 14:19:24 +0100

    first commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http :  HttpClient) { }
+
+  getAllContenido() {
+    return this.http.get('http://localhost:8080/api/demo');
+  }
+}
